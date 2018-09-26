109e3153652c3521a7e75b23c6ede6b8d4f4730c
Z003ZDBC <shiqiang.yu@siemens.com>
2018-09-26 18:05:14 +0800

    测试批量删除

--- src/main/java/com/siemens/service/ModelServiceImpl.java
+++ src/main/java/com/siemens/service/ModelServiceImpl.java
@@ -2,11 +2,13 @@ package com.siemens.service;
 
 import com.siemens.dao.ModelDao;
 import com.siemens.model.Model;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -47,7 +49,14 @@ public class ModelServiceImpl implements ModelService {
 
     @Override
     public void deleteMulti(List<Long> ids) {
-//        modelDao.delete(models);
+//        List<Model> list = new ArrayList<>();
+//        Model model1 = new Model();
+//        Model model2 = new Model();
+//        model1.setId(8L);
+//        model2.setId(9L);
+//        list.add(model1);
+//        list.add(model2);
+//        modelDao.deleteAll(list);
         for (Long id : ids) {
             modelDao.deleteById(id);
         }
