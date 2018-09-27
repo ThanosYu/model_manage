447fd337e3ceef6a758f1df958f9fc59874ee90e
Z003ZDBC <shiqiang.yu@siemens.com>
2018-09-27 17:48:22 +0800

    还原log配置

--- src/main/java/com/siemens/controller/ModelController.java
+++ src/main/java/com/siemens/controller/ModelController.java
@@ -4,9 +4,6 @@ import com.siemens.model.Model;
 import com.siemens.response.Result;
 import com.siemens.response.Status;
 import com.siemens.service.ModelService;
-import org.apache.log4j.Level;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.data.domain.Page;
@@ -211,26 +208,4 @@ public class ModelController {
         return result;
     }
 
-    @RequestMapping(value = "/log/{level}", method = {RequestMethod.GET}, consumes = "application/json", produces = "application/json")
-    @ResponseBody
-    public Result log(@PathVariable String level) {
-        Result<Optional<Model>> result = new Result<Optional<Model>>();
-        result.setCode(Status.LSucceed.getCode());
-        result.setMsg(Status.LSucceed.getMessage());
-        try {
-            Logger logger = LogManager.getRootLogger();
-            System.out.println("---------------------------------------current level: "+logger.getLevel());
-            logger.info("----------------------------------------------INFO");
-            logger.debug("---------------------------------------------DEBUG");
-            logger.error("---------------------------------------------ERROR");
-
-            Level l = Level.toLevel(level);
-            logger.setLevel(l);
-        } catch (Exception e) {
-            e.printStackTrace();
-            result.setCode(Status.LFailed.getCode());
-            result.setMsg(Status.LFailed.getMessage());
-        }
-        return result;
-    }
 }
